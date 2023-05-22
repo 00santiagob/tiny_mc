26abd87ef737854843f2121ba1ada3c7f90a4e95
nicolaMaddalozzo <s293083@studenti.polito.it>
2023-05-22 19:15:08 -0300

    version 2 del codigo con intrinsic, todavua no funciona

--- src/tiny_mc_lab2_mtwister_intr.c
+++ src/tiny_mc_lab2_mtwister_intr.c
@@ -16,6 +16,7 @@
 #include <stdbool.h>
 #include <immintrin.h>
 #include <limits.h>
+#include "simdxorshift128plus.h"
 #include <time.h> // time
 
 #define _XOPEN_SOURCE 500       // M_PI
@@ -37,11 +38,13 @@ bool areAllFalse(bool array[], int size);
 void transformVector(__m256i x, __m256 *result);
 void sumOfSquares3(__m256 x, __m256 y, __m256 z, __m256 *result);
 void sumOfSquares2(__m256 x, __m256 y, __m256 *result);
-applyCondition(__m256 shell, float SHELLS, __m256 *result);
+void applyCondition(__m256 shell, float nShells, __m256 *result);
+
+
 // global state, heat and heat square in each shell
 static float heat[SHELLS];
 static float heat2[SHELLS];
-
+int nShells = SHELLS;
 /* Photon */
 static void photon(avx_xorshift128plus_key_t * restrict my_key) {
     // const float albedo = MU_S / (MU_S + MU_A);
@@ -68,7 +71,7 @@ static void photon(avx_xorshift128plus_key_t * restrict my_key) {
     __m256 dir_y = _mm256_set1_ps(0.0f);
     //float dir_z = 1.0f;
     //float dir_z[8] = { 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f };
-    _m256 dir_z = _mm256_set1_ps(1.0f);
+    __m256 dir_z = _mm256_set1_ps(1.0f);
     
     // Initial weight of photon
     //float weight = 1.0f;
@@ -76,8 +79,11 @@ static void photon(avx_xorshift128plus_key_t * restrict my_key) {
     __m256 weight = _mm256_set1_ps(1.0f);
     
     //bool flags[8] = {true, true, true, true, true, true, true, true};
-    __m256i flags = _mm256_set1_epi32(0xFFFFFFFF);
-        
+    __m256i flags = _mm256_set1_epi32(0);
+    __m256 mask_w = _mm256_set1_ps(0.0f);
+    __m256 mask_exit = _mm256_set1_ps(0.0f);
+    __m256 cond = _mm256_set1_ps(1.0f);
+
     //bool stop = false;
     for (;;) {
     // for (int i = 0; i<N_MAX_FOR; ++i) {
@@ -121,9 +127,9 @@ static void photon(avx_xorshift128plus_key_t * restrict my_key) {
         //     shell[k] = sqrtf(x[k] * x[k] + y[k] * y[k] + z[k] * z[k]) * shells_per_mfp;
         //unsigned int shell = sqrtf(x * x + y * y + z * z) * shells_per_mfp;
         			//}
-        _m256 rad;			
+        __m256 rad;			
         sumOfSquares3(x, y, z, &rad);
-        _m256 shell = _mm256_mul_ps(rad, _m256_set1_ps(shells_per_mfp);
+        __m256 shell = _mm256_mul_ps(rad, _mm256_set1_ps(shells_per_mfp));
         
         
         			
@@ -141,16 +147,16 @@ static void photon(avx_xorshift128plus_key_t * restrict my_key) {
          //   shell[k] = ((SHELLS - 1) * (shell[k] > SHELLS - 1)) + (shell[k] * (shell[k] <= SHELLS - 1));
            //                     }
        __m256 shell_c;   
-       applyCondition(shell, (float)SHELLS, &shell_c);                   
+       applyCondition(shell, (float)nShells, &shell_c);                   
       
-      unsigned int shell_u[8];                          
-      _mm256_storeu_ps(shell_u, shell_c);                         
+      unsigned int shell_u[8];
+    _mm256_storeu_si256((__m256i*)shell_u, (__m256i)shell_c);                      
                                 
                                 
         for(int k=0; k < 8; ++k){
             //variabile = condizione ? valore_se_vero : valore_se_falso;
-            heat[shell_c[k]] = _mm256_extract_epi32(mask, k)==1 ? heat[shell_c[k]]+(1.0f - albedo) * _mm256_extract_epi32(weight, k) : heat[shell_c[k]] ;
-            heat2[shell_c[k]] = _mm256_extract_epi32(mask, k)==1 ? heat2[shell_c[k]]+(1.0f - albedo) * (1.0f - albedo) * _mm256_extract_epi32(weight, k) * _mm256_extract_epi32(weight, k) : heat2[shell_c[k]];
+            heat[shell_u[k]] = _mm256_extract_epi32(flags, k)==1 ? heat[shell_u[k]]+(1.0f - albedo) * _mm256_extract_epi32(weight, k) : heat[shell_u[k]] ;
+            heat2[shell_u[k]] = _mm256_extract_epi32(flags, k)==1 ? heat2[shell_u[k]]+(1.0f - albedo) * (1.0f - albedo) * _mm256_extract_epi32(weight, k) * _mm256_extract_epi32(weight, k) : heat2[shell_u[k]];
              
             //heat[shell[k]] += (1.0f - albedo) * weight ;
             //heat2[shell[k]] += (1.0f - albedo) * (1.0f - albedo) * weight * weight; /* add up squares */
@@ -178,44 +184,69 @@ static void photon(avx_xorshift128plus_key_t * restrict my_key) {
         /* New direction, rejection method */
         
         //for(int k=0; k<8; ++k){
+             __m256 a = _mm256_set1_ps(2.0f/(float)RAND_MAX);
+             __m256 b = _mm256_set1_ps(-1.0f);
+             __m256 t;
+                 __m256i mask = _mm256_set1_epi32(-1);
         do {
         // for(int j = 0; (j < N_MAX_WHILE) && (1.0f < t); ++j) {
              __m256i t_g = avx_xorshift128plus(my_key);
-             __m256 t;
+
              transformVector(t_g, &t);
-             __m256 a = _mm256_set1_ps(2.0f/(float)RAND_MAX);
-             __m256 b = _mm256_set1_ps(-1.0f);
+	     __mm256 xi1_new = __mm256_add_ps(b, _mm256_mul_ps(a,t))       
              
             //xi1[k] = 2.0f * genRngMTInt(rand) / (float)RAND_MAX - 1.0f;
-            xi1 = __mm256_add_ps(b, _mm256_mul_ps(a,t))
+
             __m256i t_g = avx_xorshift128plus(my_key);
-             __m256 t;
+
              transformVector(t_g, &t);
             //xi2[k] = 2.0f * genRngMTInt(rand) / (float)RAND_MAX - 1.0f;
-            xi2 = __mm256_add_ps(b, _mm256_mul_ps(a,t))
+            __mm256 xi2_new = __mm256_add_ps(b, _mm256_mul_ps(a,t))
+            
+            xi1 = _mm256_blendv_ps(xi1, xi1_new, _mm256_castsi256_ps(mask));
+            xi2 = _mm256_blendv_ps(xi2, xi2_new, _mm256_castsi256_ps(mask));
+            
+            t = sumOfSquares2(xi1, xi2);
+            __m256i update_mask = _mm256_cmp_ps(t, threshold, _CMP_LE_OQ);
+            mask = _mm256_and_si256(mask, update_mask);
+            int mask_int = _mm256_movemask_ps(_mm256_castsi256_ps(mask));
+        if (mask_int == 0xFF)
+            break;
         
-            t = sumOfSquares2(xi1, xi2) ;
-        
-        
-        } while (1.0f < t[k]);
-                              }
+        } while (true);
+                              
         // };
         
-        for(int k=0; k<8; ++k){
-        dir_x[k] = 2.0f * t[k] - 1.0f;
-        dir_y[k] = xi1[k] * sqrtf((1.0f - dir_x[k] * dir_x[k]) / t[k]);
-        dir_z[k] = xi2[k] * sqrtf((1.0f - dir_x[k] * dir_x[k]) / t[k]);
-                              }
+        //for(int k=0; k<8; ++k){
+        //dir_x[k] = 2.0f * t[k] - 1.0f;
+        //dir_y[k] = xi1[k] * sqrtf((1.0f - dir_x[k] * dir_x[k]) / t[k]);
+        //dir_z[k] = xi2[k] * sqrtf((1.0f - dir_x[k] * dir_x[k]) / t[k]);
+          //                    }
+        
+        dir_x = _mm256_add_ps(_mm256_mul_ps(_mm256_set1_ps(2.0f), t),_mm256_set1_ps(-1.0f));
+        dir_y = _mm256_mul_ps(xi1, _mm256_sqrt_ps(_mm256_div_ps(_mm256_add_ps(_mm256_mul_ps(dir_x, dir_x), _mm256_set1_ps(-1.0f)), t));          
+        dir_z = _mm256_mul_ps(xi2, _mm256_sqrt_ps(_mm256_div_ps(_mm256_add_ps(_mm256_mul_ps(dir_x, dir_x), _mm256_set1_ps(-1.0f)), t));          
+          
+          
+          
         /* Roulette */
-        for(int k=0; k<8; ++k){
-        if (weight[k] < 0.005f) {
-            if (genRngMTInt(rand) / (float)RAND_MAX > 0.1f) {
-                flags[k] = false;
-            };
-            weight[k] /= 0.1f;
-        }
+       mask_w = _mm256_cmp_ps(weight, _mm256_set1_ps(0.005f), _CMP_LT_OQ);
+       __m256i t_g = avx_xorshift128plus(my_key);
+       transformVector(t_g, &t);
+       mask_exit = _mm256_cmp_ps(_mm256_div_ps(t_g, _mm256_set1_ps((float)RAND_MAX)), _mm256_set1_ps(0.005f), _CMP_GT_OQ); 
+       flags = _mm256_or_ps(_mm256_and_ps(mask1, mask2), cond);
+       if _mm256_cmp_ps(flags, _mm256_set1_ps(1.0f), _CMP_EQ_OQ){break;}
+        //for(int k=0; k<8; ++k){
+        //if (weight[k] < 0.005f) {
+            //if (genRngMTInt(rand) / (float)RAND_MAX > 0.1f) {
+              //  flags[k] = false;
+            //};
+          //  weight[k] /= 0.1f;
+        //}
                                }
-       stop = areAllFalse(flags, 8);                        
+      // stop = areAllFalse(flags, 8);                        
+     
+     
                                
     };
 }
@@ -262,8 +293,8 @@ void sumOfSquares2(__m256 x, __m256 y, __m256 *result) {
     *result = _mm256_sqrt_ps(sum);
 }
 
-void applyCondition(__m256 shell, float SHELLS, __m256 *result) {
-    __m256 shellsMinusOne = _mm256_set1_ps(SHELLS - 1);
+void applyCondition(__m256 shell, float nShells, __m256 *result) {
+    __m256 shellsMinusOne = _mm256_set1_ps(nShells - 1);
 
     // Esegui la verifica su ogni elemento di shell
     __m256 cmpResult = _mm256_cmp_ps(shell, shellsMinusOne, _CMP_GT_OS);
